1624e21129c8309c3bc4849220e548382069ce5b
Nasrul809 <hadinasrul809@gmail.com>
2025-06-12 08:46:13 +0700

    UI Login & Dasbor

---
+++ src/alumni202457201003/Alumni202457201003.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alumni202457201003;
+
+/**
+ *
+ * @author Hype GLK
+ */
+public class Alumni202457201003 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
